7cae6776d728a749f55a33169d5ebcf7fa606601
tracer-ics2017 <tracer@njuics.org>
2025-03-06 11:38:31 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:38:31 up  2:00,  4 users,  load average: 0.04, 0.03, 0.00 541203f1d1d9964c268d6755ac3dde9fa47c24d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -98,8 +98,8 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1)
 }
 
 #define make_rtl_setget_eflags(f)                                           \
-    static inline void concat(rtl_set_, f)(const rtlreg_t* src) { TODO(); } \
-    static inline void concat(rtl_get_, f)(rtlreg_t * dest) { TODO(); }
+    static inline void concat(rtl_set_, f)(const rtlreg_t* src) { cpu.f = *src; } \
+    static inline void concat(rtl_get_, f)(rtlreg_t * dest) { *dest = cpu.f; }
 
 make_rtl_setget_eflags(CF) make_rtl_setget_eflags(OF) make_rtl_setget_eflags(ZF) make_rtl_setget_eflags(SF)
 
