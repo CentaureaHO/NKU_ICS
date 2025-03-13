990dc1aa41499a475943384f29097ffa9a5a23d0
tracer-ics2017 <tracer@njuics.org>
2025-03-13 15:14:01 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:14:01 up  1:24,  2 users,  load average: 0.04, 0.05, 0.05 c6422e6b56f2bbef3438fdf6ef4f6a85ce505c47

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -37,7 +37,9 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode)
             rtl_get_ZF(&t1);
             rtl_or(dest, &t0, &t1);
             break;
-        case CC_S: TODO();
+        case CC_S:  // sign 
+            rtl_get_SF(dest);
+            break;
         case CC_L:
             rtl_get_SF(&t0);
             rtl_get_OF(&t1);
