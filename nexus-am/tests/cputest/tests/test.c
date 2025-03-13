76e33314b74f2f63add3b245964885abc45bd3f8
tracer-ics2017 <tracer@njuics.org>
2025-03-13 13:54:24 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:54:24 up 4 min,  2 users,  load average: 0.04, 0.23, 0.13 dadd1639e83412a642dd177b785608c989282b31

--- nexus-am/tests/cputest/tests/test.c
+++ nexus-am/tests/cputest/tests/test.c
@@ -1,9 +1,88 @@
 #include "trap.h"
 
+long long sub(long long a, long long b)
+{
+    long long c = a - b;
+    return c;
+}
+
+long long test_data[] = {0,
+    1,
+    2,
+    0x7fffffffffffffffLL,
+    0x8000000000000000LL,
+    0x8000000000000001LL,
+    0xfffffffffffffffeLL,
+    0xffffffffffffffffLL};
+long long ans[] = {0LL,
+    0xffffffffffffffffLL,
+    0xfffffffffffffffeLL,
+    0x8000000000000001LL,
+    0x8000000000000000LL,
+    0x7fffffffffffffffLL,
+    0x2LL,
+    0x1LL,
+    0x1LL,
+    0LL,
+    0xffffffffffffffffLL,
+    0x8000000000000002LL,
+    0x8000000000000001LL,
+    0x8000000000000000LL,
+    0x3LL,
+    0x2LL,
+    0x2LL,
+    0x1LL,
+    0LL,
+    0x8000000000000003LL,
+    0x8000000000000002LL,
+    0x8000000000000001LL,
+    0x4LL,
+    0x3LL,
+    0x7fffffffffffffffLL,
+    0x7ffffffffffffffeLL,
+    0x7ffffffffffffffdLL,
+    0LL,
+    0xffffffffffffffffLL,
+    0xfffffffffffffffeLL,
+    0x8000000000000001LL,
+    0x8000000000000000LL,
+    0x8000000000000000LL,
+    0x7fffffffffffffffLL,
+    0x7ffffffffffffffeLL,
+    0x1LL,
+    0LL,
+    0xffffffffffffffffLL,
+    0x8000000000000002LL,
+    0x8000000000000001LL,
+    0x8000000000000001LL,
+    0x8000000000000000LL,
+    0x7fffffffffffffffLL,
+    0x2LL,
+    0x1LL,
+    0LL,
+    0x8000000000000003LL,
+    0x8000000000000002LL,
+    0xfffffffffffffffeLL,
+    0xfffffffffffffffdLL,
+    0xfffffffffffffffcLL,
+    0x7fffffffffffffffLL,
+    0x7ffffffffffffffeLL,
+    0x7ffffffffffffffdLL,
+    0LL,
+    0xffffffffffffffffLL,
+    0xffffffffffffffffLL,
+    0xfffffffffffffffeLL,
+    0xfffffffffffffffdLL,
+    0x8000000000000000LL,
+    0x7fffffffffffffffLL,
+    0x7ffffffffffffffeLL,
+    0x1LL,
+    0LL};
+
+#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
+
 int main()
 {
-    long long a = 0, b = 2;
-    long long c = 0xfffffffffffffffeLL;
 
-    nemu_assert((a - b) == c);
+    nemu_assert(sub(test_data[0], test_data[2]) == ans[2]);
 }
