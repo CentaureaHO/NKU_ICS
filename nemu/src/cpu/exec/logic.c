cb468fcf1b722876f0acd63262fb5a9ae477caca
tracer-ics2017 <tracer@njuics.org>
2025-03-14 22:07:00 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:07:00 up  8:22,  4 users,  load average: 0.16, 0.09, 0.08 2b53f6c4d9d139507b200545bf1d705a2830d8f7

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -73,15 +73,18 @@ make_EHelper(sar)
     rtl_li(r0, id_dest->val);
     rtl_li(r1, id_src->val);
 
-    rtl_get_OF(r2);         // CF = old_CF || ((src != 0) && ((dest >> src) & 0x1))
-                            // OF = old_OF && (src != 1)
+    /*
+    rtl_get_OF(r2);
     rtl_subi(r3, r1, 0x1);
     Log("Got t3 = 0x%x in rtl_subi", t3);
     rtl_neq0(r3, r3);
     rtl_and(r2, r2, r3);
     rtl_set_OF(r2);
+    */
+    rtl_set_OF(disable);
 
-    rtl_mtb(r2, r0, t1);    // r2 = ((dest >> src) & 0x1)
+    rtl_subi(r2, r1, 0x1);
+    rtl_mtb(r2, r0, t2);    // r2 = ((dest >> (src - 1)) & 0x1)
     Log("Got t2 = 0x%x in rtl_mtb", t2);
     rtl_neq0(r3, r1);
     rtl_and(r2, r2, r3);
