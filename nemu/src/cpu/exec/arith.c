bcf7822d81ded3134b8b31007c89cc8361db828f
tracer-ics2017 <tracer@njuics.org>
2025-03-14 20:50:54 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:50:54 up  7:06,  4 users,  load average: 0.02, 0.05, 0.07 c8c7dd66fa51ed61487c247cab84d6f2e1d622

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -266,6 +266,8 @@ make_EHelper(sbb)
 
     rtl_update_PFZFSF(r3, id_dest->width);
 
+    operand_write(id_dest, r3);
+
     rtl_xor(r0, dest, src);     // r0 = dest ^ src
     rtl_xor(r1, dest, r3);      // r1 = dest ^ (dest - src - CF)
     rtl_and(r0, r0, r1);        // r0 = (dest ^ src) & (dest ^ (dest - src - CF))
