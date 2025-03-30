8b898a254fac0a48a71b691e4f6474ac92572a6f
tracer-ics2017 <tracer@njuics.org>
2025-03-30 22:03:28 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:03:28 up 10:20,  2 users,  load average: 0.07, 0.03, 0.01 8280349fc33b38d6ba760592ba5b033fecb338c

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -1,6 +1,30 @@
 #include <am.h>
 #include <x86.h>
 
+void print(const char *s) 
+{
+    for (; *s; s++) _putc(*s);
+}
+
+void print_hex(uint32_t val) 
+{
+    char hex_chars[] = "0123456789ABCDEF";
+    print("0x");
+    for (int i = 7; i >= 0; i--) _putc(hex_chars[(val >> (i * 4)) & 0xF]);
+}
+
+void print_dec(int val) 
+{
+  if (val < 0) 
+  {
+    _putc('-');
+    val = -val;
+  }
+  
+  if (val >= 10) print_dec(val / 10);
+  _putc('0' + (val % 10));
+}
+
 static _RegSet *(*H)(_Event, _RegSet *) = NULL;
 
 void vecsys();
@@ -10,6 +34,8 @@ _RegSet *irq_handle(_RegSet *tf) {
   _RegSet *next = tf;
   if (H) {
     _Event ev;
+    print("irq_handle: ");
+    print_hex(tf->irq);
     switch (tf->irq) {
     case 0x80:
       ev.event = _EVENT_SYSCALL;
