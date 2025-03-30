1421583ec5d6c8721986abd07708d99c063afa9b
tracer-ics2017 <tracer@njuics.org>
2025-03-30 21:50:13 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:50:13 up 10:07,  2 users,  load average: 0.00, 0.00, 0.00 94d3f8c756146d7d972240025747dd9d3e099626

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -30,32 +30,6 @@ _RegSet *irq_handle(_RegSet *tf) {
 
 static GateDesc idt[NR_IRQ];
 
-#include <am.h>
-
-void print(const char *s) {
-  for (; *s; s++) {
-    _putc(*s);
-  }
-}
-
-void print_hex(uint32_t val) {
-  char hex_chars[] = "0123456789ABCDEF";
-  _putc('0'); _putc('x');
-  for (int i = 7; i >= 0; i--) {
-    _putc(hex_chars[(val >> (i * 4)) & 0xF]);
-  }
-}
-
-void print_dec(int val) {
-  if (val < 0) {
-    _putc('-');
-    val = -val;
-  }
-  
-  if (val >= 10) print_dec(val / 10);
-  _putc('0' + (val % 10));
-}
-
 void _asye_init(_RegSet *(*h)(_Event, _RegSet *)) {
   // initialize IDT
   for (unsigned int i = 0; i < NR_IRQ; i++) {
@@ -64,16 +38,6 @@ void _asye_init(_RegSet *(*h)(_Event, _RegSet *)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
-
-  print("IDT address: "); print_hex((uint32_t)idt); print("\n");
-  print("IDT size: "); print_dec(sizeof(idt)); print("\n");
-  
-
-  print("IDT[0x80] before set_idt:\n");
-  print("  off_low: "); print_hex(idt[0x80].off_15_0); print("\n");
-  print("  cs: "); print_hex(idt[0x80].cs); print("\n");
-  print("  off_high: "); print_hex(idt[0x80].off_31_16); print("\n");
-
 
   set_idt(idt, sizeof(idt));
 
