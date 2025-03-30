720c730ba8d10a75e3195dd786aa5d7795cdd4d7
tracer-ics2017 <tracer@njuics.org>
2025-03-30 20:30:07 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:30:07 up  8:47,  2 users,  load average: 0.02, 0.03, 0.00 b59301e5e81e96cb9963ea1fbdeb022233becfe2

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,16 @@
 #include "common.h"
 
 static _RegSet *do_event(_Event e, _RegSet *r) {
-  switch (e.event) {
-  default:
-    panic("Unhandled event ID = %d", e.event);
-  }
+    switch (e.event) {
+    default:
+      panic("Unhandled event ID = %d", e.event);
+    }
 
   return NULL;
 }
 
-void init_irq(void) { _asye_init(do_event); }
+void init_irq(void) 
+{ 
+  Log("Initializing interrupt/exception handler...");
+    _asye_init(do_event); 
+}
