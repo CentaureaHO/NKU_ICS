58b54b83211601593d3c0539b46d34663e63959a
tracer-ics2017 <tracer@njuics.org>
2025-03-30 20:58:09 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:58:09 up  9:15,  2 users,  load average: 0.00, 0.00, 0.00 da834b83a8e37bbd710f84eb15f8c61c7639389c

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,6 +11,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
     rtl_push(&ret_addr);
     rtl_li(r0, cpu.cs);
     rtl_push(r0);
+
+    uint32_t gate_addr = cpu.idtr.base + NO * 8;
+    uint16_t tar_cs = vaddr_read(gate_addr + 2, 2);
+    uint32_t tar_offset_low = vaddr_read(gate_addr, 2);
+    uint32_t tar_offset_high = vaddr_read(gate_addr + 6, 2) << 16;
+
+    cpu.cs = tar_cs;
+    cpu.eip = tar_offset_low | tar_offset_high;
+
+    print_asm("intr %#x", NO);
 }
 
 void dev_raise_intr() {}
