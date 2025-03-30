51d82727dac6d821748e2559d26d6f64b76557dd
tracer-ics2017 <tracer@njuics.org>
2025-03-30 21:26:46 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:26:46 up  9:43,  2 users,  load average: 0.03, 0.04, 0.00 f7d2b8dfc1e957c8ad7b3dd3f43b270a5f08b74b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -12,6 +12,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
     rtl_li(r0, cpu.cs);
     rtl_push(r0);
 
+    Log("IDTR: base: 0x%x, limit: 0x%x", cpu.idtr.base, cpu.idtr.limit);
+
     uint32_t gate_addr = cpu.idtr.base + NO * 8;
     uint32_t tar_eip_low = vaddr_read(gate_addr, 4) & 0xffff;
     uint32_t tar_eip_high = vaddr_read(gate_addr + 4, 4) & 0xffff0000;
