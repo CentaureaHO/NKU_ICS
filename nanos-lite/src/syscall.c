805bfb455ed196b0c8e26a215503979823eeb5e9
tracer-ics2017 <tracer@njuics.org>
2025-04-03 15:56:02 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:56:02 up  1:48,  3 users,  load average: 0.05, 0.03, 0.00 f9d398964d5c80004c79b948ceb125a65b3b4ddb

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,13 @@
 #include "syscall.h"
 #include "common.h"
 
-static inline _RegSet* sys_none(_RegSet* r)
+static inline _RegSet* sys_none(_RegSet* r)   // 0
 {
   SYSCALL_ARG1(r)=1;
   return NULL;
 }
 
-static inline _RegSet* sys_exit(_RegSet* r)
+static inline _RegSet* sys_exit(_RegSet* r)   // 4
 {
   uintptr_t ret = SYSCALL_ARG2(r);
   _halt(ret);
@@ -17,7 +17,7 @@ static inline _RegSet* sys_exit(_RegSet* r)
 _RegSet *do_syscall(_RegSet *r) {
   switch (SYSCALL_ARG1(r)) {
 #define HANDLER_Y(name) return sys_##name(r);
-#define HANDLER_N(name) TODO();
+#define HANDLER_N(name) panic("Unhandled syscall ID = %d", SYSCALL_ARG1(r));
 #define X(name, idx, done) \
   case SYS_##name:         \
     HANDLER_##done(name)
