64ed6f48569d4c7c4c3b20698a0b275d343576d2
tracer-ics2017 <tracer@njuics.org>
2025-04-12 01:51:03 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:51:03 up  1:47,  4 users,  load average: 0.07, 0.02, 0.00 575941a3c37dd4e7c43e0d9be65291d326b0a8fc

--- navy-apps/libs/libos/src/syscall.h
+++ navy-apps/libs/libos/src/syscall.h
@@ -2,26 +2,26 @@
 #define __SYSCALL_H__
 
 #define SYSCALLS                                                               \
-  X(none, -1, Y)                                                                \
-  X(open, 0, N)                                                                \
-  X(read, 1, N)                                                                \
-  X(write, 2, Y)                                                               \
-  X(exit, 3, Y)                                                                \
-  X(kill, 4, N)                                                                \
-  X(getpid, 5, N)                                                              \
-  X(close, 6, N)                                                               \
-  X(lseek, 7, N)                                                               \
-  X(brk, 8, N)                                                                 \
-  X(fstat, 9, N)                                                              \
-  X(time, 10, N)                                                               \
-  X(signal, 11, N)                                                             \
-  X(execve, 12, N)                                                             \
-  X(fork, 13, N)                                                               \
-  X(link, 14, N)                                                               \
-  X(unlink, 15, N)                                                             \
-  X(wait, 16, N)                                                               \
-  X(times, 17, N)                                                              \
-  X(gettimeofday, 18, N)
+  X(none, 0, Y)                                                                \
+  X(open, 1, N)                                                                \
+  X(read, 2, N)                                                                \
+  X(write, 3, Y)                                                               \
+  X(exit, 4, Y)                                                                \
+  X(kill, 5, N)                                                                \
+  X(getpid, 6, N)                                                              \
+  X(close, 7, N)                                                               \
+  X(lseek, 8, N)                                                               \
+  X(brk, 9, N)                                                                 \
+  X(fstat, 10, N)                                                              \
+  X(time, 11, N)                                                               \
+  X(signal, 12, N)                                                             \
+  X(execve, 13, N)                                                             \
+  X(fork, 14, N)                                                               \
+  X(link, 15, N)                                                               \
+  X(unlink, 16, N)                                                             \
+  X(wait, 17, N)                                                               \
+  X(times, 18, N)                                                              \
+  X(gettimeofday, 19, N)
 
 enum {
 #define X(name, idx, done) SYS_##name = idx,
