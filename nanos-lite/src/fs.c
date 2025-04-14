9a8bd59db13e94c09a4871a76aa31f9eae8ecf40
tracer-ics2017 <tracer@njuics.org>
2025-04-14 17:21:11 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:21:11 up  1:27,  3 users,  load average: 0.03, 0.05, 0.01 6d3451c637149ee6b7f35c6632ad50436fc15164

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -37,6 +37,7 @@ static Finfo file_table[] __attribute__((used)) = {
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
@@ -101,11 +102,28 @@ ssize_t fs_write(int fd, const void *buf, size_t len)
     case FD_STDERR:
       for (size_t i = 0; i < len; i++) _putc(((char *)buf)[i]);
       return len;
+    case FD_FB:
+      panic("Not implemented for read FD_FB");
+    case FD_EVENTS:
+      panic("Not implemented for read FD_EVENTS");
+    case FD_DISPINFO:
+      panic("Not implemented for read FD_DISPINFO");
+    case FD_NORMAL:
+      panic("Not implemented for read FD_NORMAL");
     default:
-      panic("Not implemented: write to %s", file_table[fd].name);
+      break;
   }
 
-  return -1;
+  size_t remain_bytes = file_table[fd].size - file_table[fd].open_offset;
+  if (remain_bytes < len) len = remain_bytes;
+  
+  if (len > 0)
+  {
+    ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += len;
+  }
+  
+  return len;
 }
 
 off_t   fs_lseek(int fd, off_t offset, int whence)
