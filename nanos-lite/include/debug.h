1611f483f3951c01458cba3f8210bc0ce8d151a7
tracer-ics2017 <tracer@njuics.org>
2025-04-15 11:21:35 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:21:35 up  1:31,  3 users,  load average: 0.65, 0.65, 0.37 33cd7e09a21132e11f529b3d24ef5f323db7b475

--- nanos-lite/include/debug.h
+++ nanos-lite/include/debug.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 
+#define NDEBUG
 #ifndef NDEBUG
 #define Log(format, ...)                                                       \
   printk("\33[1;35m[%s,%d,%s] " format "\33[0m\n", __FILE__, __LINE__,         \
