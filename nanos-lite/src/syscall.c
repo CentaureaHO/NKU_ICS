a1961ba407c1aa5d5c374206f97e3ad68563f009
tracer-ics2017 <tracer@njuics.org>
2025-04-15 10:26:10 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:26:10 up 36 min,  3 users,  load average: 0.03, 0.14, 0.09 536cee61f40a84a98b0f4a69d14b2ca0fc197272

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,6 +25,7 @@ static inline _RegSet *sys_read(_RegSet *r) // 2
   size_t len = SYSCALL_ARG4(r);
 
   SYSCALL_ARG1(r) = fs_read(fd, buf, len);
+  Log("sys_read returns %d", SYSCALL_ARG1(r));
 
   return NULL;
 }
