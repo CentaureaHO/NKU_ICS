f29ab22ea774d021a28553498bdbdff445b82756
tracer-ics2017 <tracer@njuics.org>
2025-04-15 10:44:41 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:44:41 up 54 min,  3 users,  load average: 0.42, 0.48, 0.28 6ac942bc9deb238ea3342086a0f2624764ee9fab

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,6 +82,7 @@ ssize_t fs_read(int fd, void *buf, size_t len)
     case FD_FB:
       panic("Not implemented for read FD_FB");
     case FD_EVENTS:
+    Log("Reading events");
       read_len = events_read(buf, len);
       return read_len;
     case FD_DISPINFO:
