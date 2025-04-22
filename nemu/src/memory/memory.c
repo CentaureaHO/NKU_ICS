5306b3e369fae45d86e86c90015ba417c5bdd29c
tracer-ics2017 <tracer@njuics.org>
2025-04-22 11:03:16 +0800

    >  compile 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:03:16 up 57 min,  2 users,  load average: 0.00, 0.01, 0.00 67d81958b5044e273b5620d4b3088f0597536e73

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define pmem_rw(addr, type)                                                         \
     *(type*)({                                                                      \
@@ -8,6 +9,47 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
+
+paddr_t page_translate(vaddr_t vaddr, bool is_write)
+{
+    if (!cpu.PG) return vaddr;
+
+    uint32_t dir_index = (vaddr >> 22) & 0x3FF;
+    uint32_t page_index = (vaddr >> 12) & 0x3FF;
+    uint32_t offset = vaddr & 0xFFF;
+
+    uint32_t pde_base = cpu.PDBR;
+    PDE pde;
+    pde.val = paddr_read(pde_base + dir_index * sizeof(PDE), sizeof(PDE));
+    
+    if (!pde.present)
+        Assert(0, "Page Directory Entry not present, vaddr = 0x%x, dir_index = %d", 
+            vaddr, dir_index);
+    
+    if (pde.accessed == 0) 
+    {
+        pde.accessed = 1;
+        paddr_write(pde_base + dir_index * sizeof(PDE), sizeof(PDE), pde.val);
+    }
+
+    uint32_t pte_base = pde.page_frame << 12;
+
+    PTE pte;
+    pte.val = paddr_read(pte_base + page_index * sizeof(PTE), sizeof(PTE));
+    
+    if (!pte.present) 
+        Assert(0, "Page Table Entry not present, vaddr = 0x%x, dir_index = %d, page_index = %d", 
+            vaddr, dir_index, page_index);
+
+    if (pte.accessed == 0 || (is_write && pte.dirty == 0)) 
+    {
+        if (pte.accessed == 0) pte.accessed = 1;
+        if (is_write) pte.dirty = 1;
+        paddr_write(pte_base + page_index * sizeof(PTE), sizeof(PTE), pte.val);
+    }
+
+    return (pte.page_frame << 12) | offset;
+}
 
 /* Memory accessing interfaces */
 
@@ -32,6 +74,18 @@ void paddr_write(paddr_t addr, int len, uint32_t data)
     mmio_write(addr, len, data, mmio_id);
 }
 
-uint32_t vaddr_read(vaddr_t addr, int len) { return paddr_read(addr, len); }
+uint32_t vaddr_read(vaddr_t addr, int len)
+{
+    if ((addr & 0xFFF) + len > 0x1000) assert(0);
 
-void vaddr_write(vaddr_t addr, int len, uint32_t data) { paddr_write(addr, len, data); }
+    paddr_t paddr = page_translate(addr, false);
+    return paddr_read(paddr, len);
+}
+
+void vaddr_write(vaddr_t addr, int len, uint32_t data)
+{
+    if ((addr & 0xFFF) + len > 0x1000) assert(0);
+
+    paddr_t paddr = page_translate(addr, true);
+    paddr_write(paddr, len, data);
+}
