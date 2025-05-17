4754760428a0ed732a47f29dc61467356a82bbd3
tracer-ics2017 <tracer@njuics.org>
2025-05-17 17:48:20 +0800

    >  run 2210878 ics2210878 Linux nkuics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:48:20 up 17:52,  2 users,  load average: 0.31, 0.17, 0.06 b9cde8869741401f934b2366e26bffdff5f73ea

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,7 +37,7 @@ _RegSet* schedule(_RegSet* prev)
     static size_t i = 0;
 
     ++i;
-    if (i % 1000000) current = &pcb[0];
+    if (i % 1000) current = &pcb[0];
     else current = &pcb[1];
 //    current                  = &pcb[0];
     
